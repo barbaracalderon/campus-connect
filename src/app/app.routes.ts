55dd76afd5fcb90b642de0c7f99f4bcc90713351
barbaracalderon <bcalderoni.ti@gmail.com>
2024-07-03 02:10:16 -0300

    feat: create shared components and unique components

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,15 @@
 import { Routes } from '@angular/router';
+import { LoginComponent } from './login/login.component';
+import { HomeComponent } from './home/home.component';
+import { DisciplinasComponent } from './disciplinas/disciplinas.component';
+import { AlunosComponent } from './alunos/alunos.component';
+import { CadastroDeAlunoComponent } from './cadastro-de-aluno/cadastro-de-aluno.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+     { path: '', redirectTo: '/login', pathMatch: 'full' },
+    { path: 'login', component: LoginComponent },
+    { path: 'home', component: HomeComponent },
+    { path: 'alunos', component: AlunosComponent },
+    { path: 'cadastro-de-aluno', component: CadastroDeAlunoComponent },
+    { path: 'disciplinas', component: DisciplinasComponent }
+];
