2c52cf976e713de2570e83dccdcf786f34ae40a4
barbaracalderon <bcalderoni.ti@gmail.com>
2024-07-20 00:23:51 -0300

    feat: add bootstrap table

--- src/app/disciplinas/disciplinas.component.ts
+++ src/app/disciplinas/disciplinas.component.ts
@@ -19,24 +19,15 @@ export class DisciplinasComponent implements OnInit {
 
   ngOnInit() {
     const emailLogado = this.authService.getLoggedInEmail();
-    console.log('Email logado:', emailLogado);
     if (emailLogado) {
       const alunos = JSON.parse(localStorage.getItem('alunos') || '[]');
-      console.log('Alunos cadastrados:', alunos);
       this.aluno = alunos.find((aluno: any) => aluno.email === emailLogado);
-      console.log('Aluno encontrado:', this.aluno);
       if (this.aluno && this.aluno.curso) {
         const cursoData = cursosDisciplinas[this.aluno.curso];
         if (cursoData) {
           this.curso = cursoData;
           this.disciplinas = this.curso.semestres;
-          console.log('Curso encontrado:', this.curso);
-          console.log('Disciplinas encontradas:', this.disciplinas);
-        } else {
-          console.log('Curso não encontrado em cursosDisciplinas.');
         }
-      } else {
-        console.log('Aluno não possui um curso válido.');
       }
     }
   }
